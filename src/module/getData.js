00f13fd5316645a48ce23767841ffdf093668cd4
Denio Nhanale <dnhanale@gmail.com>
2023-05-23 10:51:31 +0200

    Add Style to the cards

--- src/module/getData.js
+++ src/module/getData.js
@@ -1,10 +1,10 @@
-import card from './card.js'
+import card from './card.js';
 
 const shows = async () => {
-    const response = await fetch('https://api.tvmaze.com/shows');
-    const data = await response.json();
-    const result = data.slice(30, 50);
-    card(result);
-}
+  const response = await fetch('https://api.tvmaze.com/shows');
+  const data = await response.json();
+  const result = data.slice(50, 70);
+  card(result);
+};
 
 export default shows;
