b5083a4719f2777afecfabcbecd75ca5114e21bc
Denio Nhanale <dnhanale@gmail.com>
2023-05-23 11:24:05 +0200

    Add Items on The Homepage

--- src/module/getData.js
+++ src/module/getData.js
@@ -3,7 +3,7 @@ import card from './card.js';
 const shows = async () => {
   const response = await fetch('https://api.tvmaze.com/shows');
   const data = await response.json();
-  const result = data.slice(50, 70);
+  const result = data.slice(40, 60);
   card(result);
 };
 
